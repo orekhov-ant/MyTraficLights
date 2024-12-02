b5c9181d0634a1b1fa24a3171e2d727dff5f196d
Orekov Anton <orekhovant@icloud.com>
2024-12-03 00:16:19 +0500

    work with logic

--- MyTraficLights/ViewController.swift
+++ MyTraficLights/ViewController.swift
@@ -7,13 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    @IBOutlet var redLightView: UIView!
+    
+    @IBOutlet var orangeLightView: UIView!
+    
+    @IBOutlet var greenLightView: UIView!
+        
+    @IBOutlet var changeLightButton: UIButton!
+    
+    private var activeLight: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        activeLight = "none"
+        redLightView.alpha = 0.3
+        orangeLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        changeLightButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func changeLightButtonDidTapped() {
+        changeLightButton.setTitle("NEXT", for: .normal)
+        toggleLightColor()
+    }
+    
+    private func toggleLightColor() {
+        switch activeLight {
+        case "red":
+            redLightView.alpha = 0.3
+            orangeLightView.alpha = 1
+            activeLight = "orange"
+        case "orange":
+            orangeLightView.alpha = 0.3
+            greenLightView.alpha = 1
+            activeLight = "green"
+        case "green":
+            greenLightView.alpha = 0.3
+            redLightView.alpha = 1
+            activeLight = "red"
+        default:
+            redLightView.alpha = 1
+            activeLight = "red"
+        }
+        
+    }
+    
 }
 
